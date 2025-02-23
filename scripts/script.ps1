0b23db7ea34b19420af55dc652a747aa5186a1bb
Carlos-dev-G <baa4tsdev@gmail.com>
2025-02-23 13:46:47 -0300

    Fix

--- scripts/script.ps1
+++ scripts/script.ps1
@@ -1,5 +1,7 @@
-# 1 - Solicitar permisos de administrador
-if (-not ([System.Security.Principal.WindowsPrincipal] [System.Security.WindowsIdentity]::GetCurrent()).IsInRole("Administrador")) {
+# Verificar si el script se ejecuta con privilegios de administrador
+$adminRole = [System.Security.Principal.WindowsPrincipal][System.Security.WindowsIdentity]::GetCurrent()
+if (-not ($adminRole.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))) {
     Write-Host "El script necesita permisos de administrador"
     Start-Process powershell -ArgumentList "-NoExit", "-File", $PSCommandPath -Verb RunAs
+    exit
 }
