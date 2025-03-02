8bb1290e7bb0161424e3a2e9fa03e0de80fe9e9b
Guille <baa4tsdev@gmail.com>
2025-03-02 00:01:51 -0300

    Create correo.ps1

---
+++ scripts/correo.ps1
@@ -0,0 +1,9 @@
+# Ejecutar como administrador
+$app = "Microsoft.windowscommunicationsapps"
+
+Get-AppxPackage -AllUsers -Name $app | Remove-AppxPackage
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "$app*" | ForEach-Object {
+    Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName
+}
+
+Write-Host "La aplicación 'Correo' ha sido desinstalada."
