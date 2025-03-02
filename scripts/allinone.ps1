4304c73b0f74de6cdc983880d29374bfb912ea18
Guille <baa4tsdev@gmail.com>
2025-03-02 00:24:14 -0300

    Create allinone.ps1

---
+++ scripts/allinone.ps1
@@ -0,0 +1,58 @@
+# Ejecutar como administrador
+
+# Desinstalar Microsoft Edge
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/edge.ps1' -UseBasicParsing).Content
+
+# Desinstalar Cortana
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/cortana.ps1' -UseBasicParsing).Content
+
+# Desinstalar OneDrive
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/onedrive.ps1' -UseBasicParsing).Content
+
+# Desinstalar Skype
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/skype.ps1' -UseBasicParsing).Content
+
+# Desinstalar Xbox
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/xbox.ps1' -UseBasicParsing).Content
+
+# Desinstalar Visor 3D
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/visor3d.ps1' -UseBasicParsing).Content
+
+# Desinstalar Tu Teléfono
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/tu_telefono.ps1' -UseBasicParsing).Content
+
+# Desinstalar Windows Feedback Hub (Sugerencias)
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/sugerencias.ps1' -UseBasicParsing).Content
+
+# Desinstalar Contactos (Microsoft.people)
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/people.ps1' -UseBasicParsing).Content
+
+# Desinstalar Correo
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/correo.ps1' -UseBasicParsing).Content
+
+# Desinstalar Office
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/office.ps1' -UseBasicParsing).Content
+
+# Desinstalar El tiempo
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/el_tiempo.ps1' -UseBasicParsing).Content
+
+# Desinstalar Windows Maps
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/mapa.ps1' -UseBasicParsing).Content
+
+# Desinstalar OneNote
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/one_one.ps1' -UseBasicParsing).Content
+
+# Desinstalar Microsoft Solitaire Collection
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/MicrosoftSolitaireCollection.ps1' -UseBasicParsing).Content
+
+# Desinstalar Microsoft Get Help
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/GetHelp.ps1' -UseBasicParsing).Content
+
+# Desinstalar Mixed Reality
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/MixedReality.ps1' -UseBasicParsing).Content
+
+# Desinstalar Películas y TV
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/ZuneVideo.ps1' -UseBasicParsing).Content
+
+# Desinstalar Groove Music
+iex (iwr 'https://raw.githubusercontent.com/baa4ts/uninstall-ps1/refs/heads/main/scripts/Groove.ps1' -UseBasicParsing).Content
